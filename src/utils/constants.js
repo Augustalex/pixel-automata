942f7791ded8e30bfb3dc709e0ac1a191944aca0
agge <augustalexandersson@gmail.com>
2022-10-10 20:27:32 +0200

    Updated world size given new performance gains

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,8 +2,7 @@ const standard = 1440;
 // const scale = window.innerHeight / standard;
 
 export const WATER_MAX_POLLUTION = 100;
-// export const WorldWidth = 115;
-export const WorldWidth = 120;
+export const WorldWidth = 180;
 export const WorldHeight = 60;
 
 export const Tags = {
